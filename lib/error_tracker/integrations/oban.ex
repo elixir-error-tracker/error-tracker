79160b6e467dabf53ffa35f8874da7b826ee54c5
crbelaus <cristian@crbelaus.com>
2024-05-29 18:35:26 +0200

    Capture Oban errors

---
+++ lib/error_tracker/integrations/oban.ex
@@ -0,0 +1,16 @@
+defmodule ErrorTracker.Integrations.Oban do
+  def attach do
+    :telemetry.attach(__MODULE__, [:oban, :job, :exception], &handle_event/4, :no_config)
+  end
+
+  def handle_event([:oban, :job, :exception], _measurements, metadata, :no_config) do
+    %{job: job, reason: exception, stacktrace: stacktrace} = metadata
+
+    ErrorTracker.report(exception, stacktrace, %{
+      job_id: job.id,
+      job_attempt: job.attempt,
+      job_queue: job.queue,
+      job_worker: job.worker
+    })
+  end
+end
