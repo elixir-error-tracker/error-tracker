fc5709d0778c95ed0f64bbf2a926dcd17170ce69
Óscar de Arriba <oscar@dearriba.es>
2024-05-31 18:44:16 +0200

    Call Repo dynamically

--- lib/error_tracker.ex
+++ lib/error_tracker.ex
@@ -1,24 +1,20 @@
 defmodule ErrorTracker do
-  alias ErrorTracker.Repo
-
   def report(exception, stacktrace, context \\ %{}) do
     {:ok, stacktrace} = ErrorTracker.Stacktrace.new(stacktrace)
     {:ok, error} = ErrorTracker.Error.new(exception, stacktrace)
 
     error =
-      Repo.insert!(error,
+      repo().insert!(error,
         on_conflict: [set: [status: :unresolved]],
         conflict_target: :fingerprint
       )
 
     error
     |> Ecto.build_assoc(:occurrences, stacktrace: stacktrace, context: context)
-    |> Repo.insert!()
+    |> repo().insert!()
   end
 
-  def raise do
-    raise "PROBANDO PROBANDO"
-  rescue
-    e -> report(e, __STACKTRACE__)
+  def repo do
+    Application.fetch_env!(:error_tracker, :repo)
   end
 end
