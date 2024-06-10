8b3cd777fc273c57b8a2e53c978426f8e0884789
Óscar de Arriba <oscar@dearriba.es>
2024-06-10 22:02:37 +0200

    Remove unused module

--- lib/error_tracker/repo.ex
+++
@@ -1,7 +0,0 @@
-defmodule ErrorTracker.Repo do
-  def insert!(struct_or_changeset, opts \\ []) do
-    repo = Application.get_env(:error_tracker, :repo)
-
-    repo.insert!(struct_or_changeset, opts)
-  end
-end
