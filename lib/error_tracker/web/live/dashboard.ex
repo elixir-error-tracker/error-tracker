155d54564fd8bb8d6da437d68528ab9826408f87
Óscar de Arriba <oscar@dearriba.es>
2024-06-28 19:19:10 +0200

    WIP Dashboard interface

---
+++ lib/error_tracker/web/live/dashboard.ex
@@ -0,0 +1,20 @@
+defmodule ErrorTracker.Web.Live.Dashboard do
+  use ErrorTracker.Web, :live_view
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def terminate(_reason, %{assigns: %{timer: timer}}) do
+    if is_reference(timer), do: Process.cancel_timer(timer)
+
+    :ok
+  end
+
+  @impl Phoenix.LiveView
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
+end
