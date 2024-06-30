b51c9c843d9bf1cd11c9ad2e793fe2f6b582ca7d
Óscar de Arriba <oscar@dearriba.es>
2024-06-30 11:28:18 +0200

    Configure assets building

--- config/config.exs
+++ config/config.exs
@@ -7,8 +7,16 @@ if config_env() == :dev do
       args: ~w(
       --config=tailwind.config.js
       --input=css/app.css
-      --output=../priv/static/assets/app.css
+      --output=../priv/static/app.css
     ),
       cd: Path.expand("../assets", __DIR__)
+    ]
+
+  config :esbuild,
+    version: "0.21.5",
+    default: [
+      args: ~w(app.js --bundle --target=es2016 --outdir=../../priv/static),
+      cd: Path.expand("../assets/js", __DIR__),
+      env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
     ]
 end
