cf05eca28bef78fc43d020b635e167703b9676d8
Óscar de Arriba <oscar@dearriba.es>
2024-06-30 17:02:21 +0200

    Load socket config from app config

--- lib/error_tracker/web/components/layouts.ex
+++ lib/error_tracker/web/components/layouts.ex
@@ -4,11 +4,16 @@ defmodule ErrorTracker.Web.Layouts do
   @css_path :code.priv_dir(:error_tracker) |> Path.join("static/app.css")
   @js_path :code.priv_dir(:error_tracker) |> Path.join("static/app.js")
 
+  @default_docket_config %{path: "/live", transport: :websocket}
+
   embed_templates "layouts/*"
 
   def get_content(:css), do: File.read!(@css_path)
   def get_content(:js), do: File.read!(@js_path)
 
-  def get_config(:socket_path), do: "/live"
-  def get_config(:socket_transport), do: :websocket
+  def get_socket_config(key) do
+    default = Map.get(@default_docket_config, key)
+    config = Application.get_env(:error_tracker, :live_view_socket, [])
+    Keyword.get(config, key, default)
+  end
 end
