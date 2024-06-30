37b8af01a753cb0b842650435d0198ede747ee4e
Óscar de Arriba <oscar@dearriba.es>
2024-06-30 17:10:03 +0200

    Add some documentation

--- lib/error_tracker/web.ex
+++ lib/error_tracker/web.ex
@@ -1,5 +1,40 @@
 defmodule ErrorTracker.Web do
-  @moduledoc false
+  @moduledoc """
+  ErrorTracker includes a Web UI to view and inspect errors occurred on your
+  application and already stored on the database.
+
+  In order to use it, you need to add the following to your Phoenix's `
+  router.ex` file:
+
+  ```elixir
+  defmodule YourAppWeb.Router do
+    use Phoenix.Router
+    use ErrorTracker.Web, :router
+
+    ...
+
+    error_tracker_dashboard "/errors"
+  end
+  ```
+
+  This will add the routes needed for the ErrorTracker LiveView UI to work.
+
+  ## LiveView socket options
+
+  By default the library expects you to have your LiveView socket at `/live` and
+  using `websocket` transport.
+
+  If that's not the case, you can configure it adding the following
+  configuration to your app's config files:
+
+  ```elixir
+  config :error_tracker,
+    socket: [
+      path: "/my-custom-socket-path"
+      transport: :longpoll # (accepted values are :longpoll or :websocket)
+    ]
+  ```
+  """
 
   def html do
     quote do
@@ -22,6 +57,12 @@ defmodule ErrorTracker.Web do
       use Phoenix.LiveComponent
 
       unquote(html_helpers())
+    end
+  end
+
+  def router do
+    quote do
+      import ErrorTracker.Web.Router
     end
   end
 
