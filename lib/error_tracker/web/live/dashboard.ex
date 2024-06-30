15a952323baec82f0b45be861c0f5cc702d5d1e6
Óscar de Arriba <oscar@dearriba.es>
2024-06-30 12:57:22 +0200

    WIP dashboard example

--- lib/error_tracker/web/live/dashboard.ex
+++ lib/error_tracker/web/live/dashboard.ex
@@ -5,18 +5,16 @@ defmodule ErrorTracker.Web.Live.Dashboard do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-
-  @impl Phoenix.LiveView
-  def terminate(_reason, %{assigns: %{timer: timer}}) do
-    if is_reference(timer), do: Process.cancel_timer(timer)
-
-    :ok
+    {:ok, assign(socket, :counter, 0)}
   end
 
   @impl Phoenix.LiveView
   def handle_params(_params, _uri, socket) do
     {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("increment", _params, socket) do
+    {:noreply, assign(socket, :counter, socket.assigns.counter + 1)}
   end
 end
