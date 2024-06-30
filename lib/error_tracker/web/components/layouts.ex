69c5262d0cab2b9994d71ba08e9574b00d582d0f
Óscar de Arriba <oscar@dearriba.es>
2024-06-30 11:55:57 +0200

    Read assets from disk and inject it on the HTML

--- lib/error_tracker/web/components/layouts.ex
+++ lib/error_tracker/web/components/layouts.ex
@@ -1,5 +1,11 @@
 defmodule ErrorTracker.Web.Layouts do
   use ErrorTracker.Web, :html
 
+  @css_path :code.priv_dir(:error_tracker) |> Path.join("static/app.css")
+  @js_path :code.priv_dir(:error_tracker) |> Path.join("static/app.js")
+
   embed_templates "layouts/*"
+
+  def get_content(:css), do: File.read!(@css_path)
+  def get_content(:js), do: File.read!(@js_path)
 end
