8c9a63833b09781da61e2ace9a413de064a2f294
Óscar de Arriba <oscar@dearriba.es>
2024-07-20 11:59:24 +0200

    Load occurrences list

--- lib/error_tracker/web/live/show.ex
+++ lib/error_tracker/web/live/show.ex
@@ -1,16 +1,66 @@
 defmodule ErrorTracker.Web.Live.Show do
   @moduledoc false
-
   use ErrorTracker.Web, :live_view
 
+  import Ecto.Query
+
   alias ErrorTracker.Error
+  alias ErrorTracker.Occurrence
 
-  def mount(params, _session, socket) do
-    error = ErrorTracker.repo().get!(Error, params["id"], prefix: ErrorTracker.prefix())
-    error = ErrorTracker.repo().preload(error, [:occurrences], prefix: ErrorTracker.prefix())
+  @occurreneces_to_navigate 50
 
-    dbg(error)
-
+  def mount(%{"id" => id}, _session, socket) do
+    error = ErrorTracker.repo().get!(Error, id, prefix: ErrorTracker.prefix())
     {:ok, assign(socket, error: error)}
+  end
+
+  def handle_params(%{"occurence_id" => occurrence_id}, _uri, socket) do
+    base_query = Ecto.assoc(socket.assigns.error, :occurrences)
+
+    occurrence =
+      ErrorTracker.repo().get!(base_query, occurrence_id, prefix: ErrorTracker.prefix())
+
+    previous_occurrences =
+      base_query
+      |> where([o], o.id < ^occurrence.id)
+      |> related_occurrences(@occurreneces_to_navigate / 2)
+
+    limit_next_occurrences = @occurreneces_to_navigate - length(previous_occurrences) - 1
+
+    next_occurrences =
+      base_query
+      |> where([o], o.id > ^occurrence.id)
+      |> related_occurrences(limit_next_occurrences)
+
+    socket =
+      socket
+      |> assign(:occurrences, previous_occurrences ++ occurrence ++ next_occurrences)
+      |> assign(:occurrence, occurrence)
+
+    {:noreply, socket}
+  end
+
+  def handle_params(_, _uri, socket) do
+    base_query = Ecto.assoc(socket.assigns.error, :occurrences)
+
+    occurrences = related_occurrences(base_query)
+
+    occurrence =
+      ErrorTracker.repo().get!(base_query, hd(occurrences).id, prefix: ErrorTracker.prefix())
+
+    socket =
+      socket
+      |> assign(:occurrences, occurrences)
+      |> assign(:occurrence, occurrence)
+
+    {:noreply, socket}
+  end
+
+  defp related_occurrences(query, num_results \\ @occurreneces_to_navigate) do
+    query
+    |> order_by([o], desc: o.id)
+    |> select([:id, :inserted_at])
+    |> limit(^num_results)
+    |> ErrorTracker.repo().all(prefix: ErrorTracker.prefix())
   end
 end
