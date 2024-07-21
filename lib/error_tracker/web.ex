d713724044b6c68554b3350e4f66a681abc618fd
Óscar de Arriba <oscar@dearriba.es>
2024-07-21 21:03:21 +0200

    Sanitize module names and format datetimes

--- lib/error_tracker/web.ex
+++ lib/error_tracker/web.ex
@@ -74,6 +74,7 @@ defmodule ErrorTracker.Web do
       import Phoenix.LiveView.Helpers
 
       import ErrorTracker.Web.CoreComponents
+      import ErrorTracker.Web.Helpers
       import ErrorTracker.Web.Router.Routes
 
       alias Phoenix.LiveView.JS
