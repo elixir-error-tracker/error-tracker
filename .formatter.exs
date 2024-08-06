1c96741de246faba41fbd345c0e20a48dc2bb096
crbelaus <cristian@crbelaus.com>
2024-08-06 16:32:28 +0200

    Allow error_tracker_dashboard without parens
    This is consistent with how other packages such as Phoenix LiveDashboard
    or Oban work when mounting resources on the router.

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,10 @@
 # Used by "mix format"
+locals_without_parens = [error_tracker_dashboard: 1, error_tracker_dashboard: 2]
+
 [
   import_deps: [:ecto, :ecto_sql, :plug, :phoenix],
   inputs: ["{mix,.formatter,dev,dev.*}.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
-  plugins: [Phoenix.LiveView.HTMLFormatter]
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
