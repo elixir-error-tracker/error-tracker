11dde5eeb4d13505e8ee29f70917ffda487a2e8f
Cristian Álvarez Belaustegui <cristian@crbelaus.com>
2024-08-06 15:21:19 +0200

    Read assets during compilation (#32)
    Read CSS and JS assets during compilation. This ensures that the CSS
    and JS files are present when the release is built and then they are not
    needed anymore as they have been compiled.

--- lib/error_tracker/web/components/layouts.ex
+++ lib/error_tracker/web/components/layouts.ex
@@ -4,18 +4,18 @@ defmodule ErrorTracker.Web.Layouts do
 
   alias ErrorTracker.Web.Layouts.Navbar
 
-  @css_path :code.priv_dir(:error_tracker) |> Path.join("static/app.css")
-  @js_path :code.priv_dir(:error_tracker) |> Path.join("static/app.js")
+  @default_socket_config %{path: "/live", transport: :websocket}
 
-  @default_docket_config %{path: "/live", transport: :websocket}
+  @css :code.priv_dir(:error_tracker) |> Path.join("static/app.css") |> File.read!()
+  @js :code.priv_dir(:error_tracker) |> Path.join("static/app.js") |> File.read!()
 
   embed_templates "layouts/*"
 
-  def get_content(:css), do: File.read!(@css_path)
-  def get_content(:js), do: File.read!(@js_path)
+  def get_content(:css), do: @css
+  def get_content(:js), do: @js
 
   def get_socket_config(key) do
-    default = Map.get(@default_docket_config, key)
+    default = Map.get(@default_socket_config, key)
     config = Application.get_env(:error_tracker, :live_view_socket, [])
     Keyword.get(config, key, default)
   end
