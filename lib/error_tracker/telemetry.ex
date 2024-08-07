d2fc4896aadaaac646647b102376ff88d74b3c67
Óscar de Arriba <oscar@dearriba.es>
2024-08-07 12:10:17 +0200

    Add telemetry documentation

--- lib/error_tracker/telemetry.ex
+++ lib/error_tracker/telemetry.ex
@@ -1,6 +1,45 @@
 defmodule ErrorTracker.Telemetry do
   @moduledoc """
-  TODO
+  Telemetry events of ErrorTracker.
+
+  ErrorTracker emits some events to allow third parties to receive information
+  of errors and occurrences stored.
+
+  We emit four type of events which allows to track the lifetime of errors of
+  your application.
+
+  ### Error events
+
+  Those occur during the lifetime of an error:
+
+  * `[:error_tracker, :error, :new]`: is emitted when a new error is stored and
+  no previous occurrences were known.
+
+  * `[:error_tracker, :error, :resolved]`: is emitted when a new error is marked
+  as resolved on the UI.
+
+  * `[:error_tracker, :error, :unresolved]`: is emitted when a new error is
+  marked as unresolved on the UI or a new occurrence is registered, moving the
+  error to the unresolved state.
+
+  ### Occurrence events
+
+  There is only one event emitted for occurrences:
+
+  * `[:error_tracker, :occurrence, :new]`: is emitted when a new occurrence is
+  stored.
+
+  ### Measures and metadata
+
+  Each event is emitted with some measures and metadata, which can be used to
+  receive information without having to query the database again:
+
+  | event                                   | measures       | metadata      |
+  | --------------------------------------- | -------------- | ------------- |
+  | `[:error_tracker, :error, :new]`        | `:system_time` | `:error`      |
+  | `[:error_tracker, :error, :unresolved]` | `:system_time` | `:error`      |
+  | `[:error_tracker, :error, :resolved]`   | `:system_time` | `:error`      |
+  | `[:error_tracker, :occurrence, :new]`   | `:system_time` | `:occurrence` |
   """
 
   @doc false
