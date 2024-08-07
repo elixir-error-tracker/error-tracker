75374381d2ae554fdb0c92cde90d9f7a87c81a97
crbelaus <cristian@crbelaus.com>
2024-08-07 18:55:31 +0200

    Which is the current version?

--- test/error_tracker_test.exs
+++ test/error_tracker_test.exs
@@ -26,7 +26,7 @@ defmodule ErrorTrackerTest do
       assert error.kind == to_string(ArithmeticError)
       assert error.reason == "bad argument in arithmetic expression"
 
-      unless dbg(Version.compare("1.7.0", System.version())) == :gt do
+      unless IO.inspect(Version.compare("1.7.0", System.version())) == :gt do
         # This only works from Elixir 1.7.0
         assert error.source_function == "erlang.+/2"
         assert error.source_line == "nofile"
