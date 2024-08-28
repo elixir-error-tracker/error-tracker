720470ca5f32db6bcbca9d58cfad2698eeff70be
Óscar de Arriba <oscar@dearriba.es>
2024-08-28 20:44:19 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ErrorTracker.MixProject do
   def project do
     [
       app: :error_tracker,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
