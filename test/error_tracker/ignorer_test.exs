f671cc6460d8297fec0bf4db5dd20c080555750a
crbelaus <cristian@crbelaus.com>
2024-09-06 16:38:26 +0200

    Improve tests

--- test/error_tracker/ignorer_test.exs
+++ test/error_tracker/ignorer_test.exs
@@ -1,12 +1,26 @@
 defmodule ErrorTracker.IgnorerTest do
   use ErrorTracker.Test.Case
 
-  setup_all do
-    Application.put_env(:error_tracker, :ignorer, ErrorTracker.EveryErrorIgnorer)
+  setup context do
+    if ignorer = context[:ignorer] do
+      previous_setting = Application.get_env(:error_tracker, :ignorer)
+      Application.put_env(:error_tracker, :ignorer, ignorer)
+      # Ensure that the application env is restored after each test
+      on_exit(fn -> Application.put_env(:error_tracker, :ignorer, previous_setting) end)
+    end
+
+    []
   end
 
-  test "ignores errors" do
+  @tag ignorer: ErrorTracker.EveryErrorIgnorer
+  test "with an ignorer ignores errors" do
     assert :noop = report_error(fn -> raise "[IGNORE] Sample error" end)
+    assert %ErrorTracker.Occurrence{} = report_error(fn -> raise "Sample error" end)
+  end
+
+  @tag ignorer: false
+  test "without an ignorer does not ignore errors" do
+    assert %ErrorTracker.Occurrence{} = report_error(fn -> raise "[IGNORE] Sample error" end)
     assert %ErrorTracker.Occurrence{} = report_error(fn -> raise "Sample error" end)
   end
 end
