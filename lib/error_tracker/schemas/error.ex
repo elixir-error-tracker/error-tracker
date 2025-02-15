b9c7e66a5f5a4ad2cea2598ea7941c6a1458a718
crbelaus <cristian@crbelaus.com>
2025-02-15 07:51:32 +0100

    Muted is false by default

--- lib/error_tracker/schemas/error.ex
+++ lib/error_tracker/schemas/error.ex
@@ -22,7 +22,7 @@ defmodule ErrorTracker.Error do
     field :status, Ecto.Enum, values: [:resolved, :unresolved], default: :unresolved
     field :fingerprint, :binary
     field :last_occurrence_at, :utc_datetime_usec
-    field :muted, :boolean
+    field :muted, :boolean, default: false
 
     has_many :occurrences, ErrorTracker.Occurrence
 
