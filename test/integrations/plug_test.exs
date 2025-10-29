c243549e62ef0a6c21a7b3e4bf8276a8c03d98d8
Tim Jarratt <tim.jarratt@maersk.com>
2025-10-29 11:43:11 +0100

    Backfill test for recording request headers

--- test/integrations/plug_test.exs
+++ test/integrations/plug_test.exs
@@ -1,18 +1,28 @@
 defmodule ErrorTracker.Integrations.PlugTest do
   use ErrorTracker.Test.Case
 
-  alias ErrorTracker.Integrations.Plug
+  alias ErrorTracker.Integrations.Plug, as: IntegrationPlug
 
   @fake_callstack []
 
-  test "it reports errors" do
-    conn = Phoenix.ConnTest.build_conn()
+  test "it reports errors, including the request headers" do
+    conn = Phoenix.ConnTest.build_conn() |> Plug.Conn.put_req_header("accept", "application/json")
 
-    Plug.report_error(conn, {"an error from Phoenix", "something bad happened"}, @fake_callstack)
+    IntegrationPlug.report_error(
+      conn,
+      {"an error from Phoenix", "something bad happened"},
+      @fake_callstack
+    )
 
     [error] = repo().all(ErrorTracker.Error)
 
     assert error.kind == "an error from Phoenix"
     assert error.reason == "something bad happened"
+
+    [occurrence] = repo().all(ErrorTracker.Occurrence)
+    assert occurrence.error_id == error.id
+
+    %{"request.headers" => request_headers} = occurrence.context
+    assert request_headers == %{"accept" => "application/json"}
   end
 end
